3b7fd4758dab6c2dafd0c3550e991730adea8263
Habashi111 <urorotevoesvkib@gmail.com>
2025-03-08 22:58:29 +0200

    تم تحديث الملفات وإصلاح الأخطاء

--- src/Components/protectedRoute/protectedRoute.jsx
+++ src/Components/protectedRoute/protectedRoute.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import "./ProtectedRoute.module.css"
+import "./protectedRoute.module.css"
 
 import { Navigate } from 'react-router-dom';
+
 export default function ProtectedRoute(props) {
 
   if(localStorage.getItem("token")){
